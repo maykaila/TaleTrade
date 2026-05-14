226f5f3302977800c6c9438c9e8051275d3773f4
maykaila <23104019@usc.edu.ph>
2026-05-14 11:03:05 +0800

    Revert "to be deleted"
    This reverts commit f95c7da108ebb5c141a981661eb9b67e3d03d700.

--- metro.config.js
+++ metro.config.js
@@ -1,23 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
- * https://docs.expo.dev/guides/customizing-metro/
+ * https://reactnative.dev/docs/metro
  *
- * @type {import('expo/metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = getDefaultConfig(__dirname);
+const config = {};
 
-module.exports = config;
-
-// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-
-// /**
-//  * Metro configuration
-//  * https://reactnative.dev/docs/metro
-//  *
-//  * @type {import('@react-native/metro-config').MetroConfig}
-//  */
-// const config = {};
-
-// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
